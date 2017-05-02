0e1f05fcafc220a9b64777ad56d8e394c2422702
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-05-02 16:52:31 -0400

    Get rid of Thing

--- db/models/favorite.js
+++ db/models/favorite.js
@@ -4,7 +4,6 @@ const {STRING} = require('sequelize')
 
 module.exports = db => db.define('favorites')
 
-module.exports.associations = (Favorite, {Thing, User}) => {
-  Favorite.belongsTo(Thing)
+module.exports.associations = (Favorite, {User}) => {
   Favorite.belongsTo(User)
 }
--- db/seed.js
+++ db/seed.js
@@ -1,13 +1,12 @@
 'use strict'
 
 const db = require('APP/db')
-    , {User, Thing, Favorite, Promise} = db
+    , {User, Favorite, Promise} = db
     , {mapValues} = require('lodash')
 
 function seedEverything() {
   const seeded = {
-    users: users(),
-    things: things(),
+    users: users()
   }
 
   seeded.favorites = favorites(seeded)
@@ -28,12 +27,6 @@ const users = seed(User, {
   },
 })
 
-const things = seed(Thing, {
-  surfing: {name: 'surfing'},
-  smiting: {name: 'smiting'},
-  puppies: {name: 'puppies'},
-})
-
 const favorites = seed(Favorite,
   // We're specifying a function here, rather than just a rows object.
   // Using a function lets us receive the previously-seeded rows (the seed
@@ -42,27 +35,23 @@ const favorites = seed(Favorite,
   // This lets us reference previously-created rows in order to create the join
   // rows. We can reference them by the names we used above (which is why we used
   // Objects above, rather than just arrays).
-  ({users, things}) => ({
+  ({users}) => ({
     // The easiest way to seed associations seems to be to just create rows
     // in the join table.
     'obama loves surfing': {
       user_id: users.barack.id,    // users.barack is an instance of the User model
                                    // that we created in the user seed above.
                                    // The seed function wires the promises so that it'll
-                                   // have been created already.
-      thing_id: things.surfing.id  // Same thing for things.
+                                   // have been created already
     },
     'god is into smiting': {
       user_id: users.god.id,
-      thing_id: things.smiting.id
     },
     'obama loves puppies': {
-      user_id: users.barack.id,
-      thing_id: things.puppies.id
+      user_id: users.barack.id
     },
     'god loves puppies': {
-      user_id: users.god.id,
-      thing_id: things.puppies.id
+      user_id: users.god.id
     },
   })
 )
@@ -135,4 +124,4 @@ function seed(Model, rows) {
   }
 }
 
-module.exports = Object.assign(seed, {users, things, favorites})
+module.exports = Object.assign(seed, {users, favorites})
