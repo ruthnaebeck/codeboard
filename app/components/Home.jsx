8812963d42542b21bf15c6fddadf1924efe0ee6b
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-05-03 21:17:23 -0400

    Updated styles to position get started button

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -6,9 +6,13 @@ import { Card, CardMedia, CardTitle } from 'material-ui/Card'
 export const Home = (props) => (
     <Card>
       <CardMedia
-        overlay={<CardTitle title="Welcome to Code Board!" subtitle="" />}>
-        <center><RaisedButton label="Get Started" /></center>
-        <img src="/images/pen-marker-hand.png" />
+        overlay={<CardTitle title="Welcome to Code Board!" subtitle=""/>}>
+        <div className="splash row"
+          style={{ backgroundImage: 'url(/images/pen-marker-hand.png)' }}>
+          <div className="col-sm-12 started">
+            <RaisedButton label="Get Started" primary={true}/>
+          </div>
+        </div>
       </CardMedia>
     </Card>
   )
