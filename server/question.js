8351de768549aaf5e1970342d52527a6c0194b45
Cigdem Aybar <aybarcigdem@gmail.com>
2017-05-05 15:51:59 -0400

    Update question.js

--- server/question.js
+++ server/question.js
@@ -1,10 +1,12 @@
 const db = require('APP/db')
 const Question = db.model('questions')
+const Synthesize = require('./textToSpeech.js')
 
 module.exports = require('express').Router()
   .get('/:id', (req, res, next) => {
     Question.findById(req.params.id)
     .then(question => {
+      Synthesize(question.text)
       res.json(question)
     })
     .catch(next)
