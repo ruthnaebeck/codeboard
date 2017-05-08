1b2739026ea900d03b4fa23ff7d6a01fbd4b85c0
Fanny Jiang <jiangf@bu.edu>
2017-05-08 12:40:23 -0400

    add Paper styling to text editor and whiteboard divs

--- app/components/Whiteboard.jsx
+++ app/components/Whiteboard.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import Paper from 'material-ui/Paper'
 
 const _ = require('lodash')
 const fp = require('lodash/fp')
@@ -23,14 +24,27 @@ export default class Whiteboard extends Component {
   }
 
   render() {
-    console.log('IS THIS PROCESS? ', process.env)
+    const style = {
+      height: '100vh',
+      width: '48vw',
+      margin: '10px 0px 10px 0px',
+      textAlign: 'center',
+      display: 'inline-block'
+    }
     return (
-      <div id="myScript">
-        <myscript-text-web id="textInput"
-          applicationkey="b3eb3c07-12df-4809-8bc5-18715cf3b24e"
-          hmackey="bc9ba480-0640-44bc-b9e5-8480e9954577"
-          language="en_US"
-          recognitioncandidates="1"></myscript-text-web>
+      <div className="row">
+        <div className="col-sm-6">
+          <Paper style={style} zDepth={3} />
+        </div>
+        <div className="col-sm-6" id="myScript">
+          <Paper style={style} zDepth={3}>
+          <myscript-text-web id="textInput"
+            applicationkey="b3eb3c07-12df-4809-8bc5-18715cf3b24e"
+            hmackey="bc9ba480-0640-44bc-b9e5-8480e9954577"
+            language="en_US"
+            recognitioncandidates="1"></myscript-text-web>
+          </Paper>
+        </div>
       </div>
     )
   }
