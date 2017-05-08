ceaa57a32157dd34a360c5c429e774c05c8c33ae
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-05-08 15:23:52 -0400

    Added react-ace text editor

--- app/components/Whiteboard.jsx
+++ app/components/Whiteboard.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import Paper from 'material-ui/Paper'
+import brace from 'brace'
+import AceEditor from 'react-ace'
+import 'brace/mode/text'
+import 'brace/theme/github'
 
 const _ = require('lodash')
 const fp = require('lodash/fp')
 const object = require('lodash/fp/object')
-const ace = require('ace-code-editor')
 
 export default class Whiteboard extends Component {
   constructor(props) {
@@ -17,38 +20,35 @@ export default class Whiteboard extends Component {
 
   componentDidMount() {
     const textInput = document.getElementById('textInput')
-    console.log('TEXT INPUT: ', textInput)
+    // console.log('TEXT INPUT: ', textInput)
     textInput.addEventListener('myscript-text-web-result', function(e) {
       const inputTextPath = _.get(e, 'detail.result.textSegmentResult.candidates[0].label', 'not found, default')
-      console.log('IS THIS OUR RESULT???', inputTextPath)
+      // console.log('IS THIS OUR RESULT???', inputTextPath)
     })
   }
 
   render() {
-    const style = {
-      height: '100vh',
-      width: '48vw',
-      margin: '10px 0px 10px 0px',
-      textAlign: 'center',
-      display: 'inline-block'
-    }
-
-    const editor = ace.edit('editor')
-
     return (
       <div className="row">
         <div className="col-sm-6">
-          <Paper style={style} zDepth={3} />
-            <div id="editor">some text</div>
-
+          <Paper className="ace" zDepth={3}>
+            <AceEditor
+              mode="text"
+              theme="github"
+              name="editor"
+              width="94%"
+              height="94%"
+              editorProps={{ $blockScrolling: true }}
+            />
+          </Paper>
         </div>
         <div className="col-sm-6" id="myScript">
-          <Paper style={style} zDepth={3}>
-          <myscript-text-web id="textInput"
-            applicationkey="b3eb3c07-12df-4809-8bc5-18715cf3b24e"
-            hmackey="bc9ba480-0640-44bc-b9e5-8480e9954577"
-            language="en_US"
-            recognitioncandidates="1"></myscript-text-web>
+          <Paper className="ace" zDepth={3}>
+            <myscript-text-web id="textInput"
+              applicationkey="b3eb3c07-12df-4809-8bc5-18715cf3b24e"
+              hmackey="bc9ba480-0640-44bc-b9e5-8480e9954577"
+              language="en_US"
+              recognitioncandidates="1"></myscript-text-web>
           </Paper>
         </div>
       </div>
