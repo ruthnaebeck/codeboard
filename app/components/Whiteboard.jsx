f967525fe8b0372f188e3171debfe0a46d41334c
Cigdem Aybar <aybarcigdem@gmail.com>
2017-05-10 12:54:59 -0400

    bottomNavBar added

--- app/components/Whiteboard.jsx
+++ app/components/Whiteboard.jsx
@@ -10,6 +10,8 @@ import AceEditor from 'react-ace'
 import 'brace/mode/text'
 import 'brace/theme/github'
 
+import BottomNavBar from './BottomNavBar'
+
 const _ = require('lodash')
 const fp = require('lodash/fp')
 const object = require('lodash/fp/object')
@@ -107,11 +109,9 @@ class Whiteboard extends Component {
               </span>
               <span
                 className="span-arrow">
-                <Play />
               </span>
               <span
                 className="span-arrow">
-                <Save />
               </span>
               <span
                 className="span-arrow"
@@ -151,6 +151,7 @@ class Whiteboard extends Component {
             </Paper>
           </div>
         </div>
+        <BottomNavBar />
       </div>
     )
   }
@@ -165,18 +166,6 @@ const LeftArrow = () => (
 const RightArrow = () => (
   <SvgIcon>
     <path d="M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z" />
-  </SvgIcon>
-)
-
-const Play = () => (
-  <SvgIcon>
-    <path d="M8 5v14l11-7z" />
-  </SvgIcon>
-)
-
-const Save = () => (
-  <SvgIcon>
-    <path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z" />
   </SvgIcon>
 )
 
