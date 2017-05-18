1375ab2753bd88af79a1932c29c5337249de6bb8
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-05-18 11:57:33 -0400

    hide mocha dive

--- app/components/Whiteboard.jsx
+++ app/components/Whiteboard.jsx
@@ -187,7 +187,7 @@ class Whiteboard extends Component {
           <div className='col-sm-12'>
             <BottomNavBar wbState={this.state} />
           </div>
-          <div id="mocha col-hide" />
+          <div id="mocha" className="col-hide" />
         </div>
       </div>
     )
